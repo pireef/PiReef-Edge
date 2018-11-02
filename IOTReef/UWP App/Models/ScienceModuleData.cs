b3896daca74f18d83bb0af7903210efddee6d1df
Brett Bowen <bjbowen4@yahoo.com>
2018-11-02 14:46:08 -0500

    Files Initially moved over and it builds

---
+++ IOTReef/UWP App/Models/ScienceModuleData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTReef_HubModule.Models
+{
+    public class ScienceModuleData
+    {
+        private DateTime timeRead;
+        private bool readSuccess;
+        private float temp;
+        private float ph;
+        private float salinity;
+        private float orp;
+        private bool converttoF;
+
+
+        public float Temp{get => temp; set => temp = (value * 9) / 5 + 32;}
+        public bool ReadSuccess { get => readSuccess; set => readSuccess = value; }
+        public DateTime TimeRead { get => timeRead; set => timeRead = value; }
+        public float PH { get => ph; set => ph = value; }
+        public float Salinity { get => salinity; set => salinity = value; }
+        public float ORP { get => orp; set => orp = value; }
+        public bool ConverttoF { get => converttoF; set => converttoF = value; }
+    }
+}
